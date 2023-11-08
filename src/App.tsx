15f15b89b509ad49e43a2b371d8ae05b8ca09a21
Andre Moreira <andre.moreira.9@hotmail.com>
2023-11-08 20:14:33 +0100

    Fix action text not centered on non 5 rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import CategoriesList from "./components/tables/CategoriesList";
 import ExerciseList from "./components/tables/ExerciseList";
 
 function App() {
   return (
     <main className="w-full h-full">
-      <ExerciseList />
+      <CategoriesList />
     </main>
   );
 }
