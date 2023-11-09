9830114520eb5297c7f87469d1f74e04a1aeea6f
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-11-09 18:24:16 +0100

    added changes to exercise page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import './index.css';
+import LoginPage from './components/LoginPage';
+// ... Import other components for your routes ...
 
 function App() {
   return (
-    <div className="flex min-h-screen">
+    <Router>
       <Sidebar />
-      <main className="flex-grow bg-white"> {/* Here we apply the bg-white class to the main content area */}
-        {/* Your main content */}
-      </main>
-    </div>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} /> // Add your ProfilePage component
+        <Route path="/exercises" element={<ExercisesPage />} /> // Add your ExercisesPage component
+        <Route path="/exercises/create" element={<CreateExercisePage />} /> // Add your CreateExercisePage component
+        <Route path="/admins" element={<AdminsPage />} /> // Add your AdminsPage component
+        <Route path="/admins/create" element={<CreateAdminPage />} /> // Add your CreateAdminPage component
+        {/* ... other routes ... */}
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,46 +1,38 @@
 import { FiLogOut, FiFileText } from 'react-icons/fi';
-import { ImProfile } from 'react-icons/im';
+import { LuUsers } from 'react-icons/lu';
+import { Link } from 'react-router-dom'; // Import the Link component
 
 const Sidebar = () => {
     return (
-        <div className="w-16 h-screen flex flex-col justify-between py-4 bg-blue-900">
+        <div className="w-16 h-screen flex flex-col justify-between py-4 bg-custom-dard-blue">
             <div>
                 {/* Square with rounded edges containing the "K" */}
                 <div className="flex justify-center mt-4 mb-8">
-                    <div className="bg-blue-100 rounded-lg w-12 h-12 flex items-center justify-center">
-                        <span className="text-blue-500 text-3xl font-bold">K</span>
+                    <div className="bg-custom-blue rounded-lg w-12 h-12 flex items-center justify-center">
+                        <span className="text-custom-blue text-3xl font-bold">K</span>
                     </div>
                 </div>
                 
                 {/* Profile icon */}
                 <div className="mb-6 flex justify-center">
-                    <button
-                        className="bg-blue-800 p-0 rounded-lg flex items-center justify-center w-12 h-12"
-                        title="Profile"
-                    >
-                        <ImProfile size={30} className="text-white" />
-                    </button>
+                    <Link to="/profile" className="bg-custom-blue p-0 rounded-lg flex items-center justify-center w-12 h-12" title="Profile">
+                        <LuUsers size={30} className="text-white" />
+                    </Link>
                 </div>
 
                 {/* Sheets icon */}
                 <div className="mb-6 flex justify-center">
-                    <button
-                        className="bg-blue-800 p-0 rounded-lg flex items-center justify-center w-12 h-12"
-                        title="Exercise List"
-                    >
+                    <Link to="/exercises" className="bg-custom-blue p-0 rounded-lg flex items-center justify-center w-12 h-12" title="Exercise List">
                         <FiFileText size={30} className="text-white" />
-                    </button>
+                    </Link>
                 </div>
             </div>
 
             {/* Logout button at the bottom */}
             <div className="flex justify-center pb-4">
-                <button
-                    className="bg-blue-800 p-0 rounded-lg flex items-center justify-center w-12 h-12"
-                    title="Logout"
-                >
+                <Link to="/login" className="bg-custom-blue p-0 rounded-lg flex items-center justify-center w-12 h-12" title="Logout">
                     <FiLogOut size={30} className="text-white" />
-                </button>
+                </Link>
             </div>
         </div>
     );
