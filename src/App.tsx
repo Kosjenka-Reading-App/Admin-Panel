f3eee7acdd2d1b91895e5d21917817dac6ff643a
André Moreira <andre.moreira.9@hotmail.com>
2023-11-12 00:24:27 +0100

    Add new routes to router and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import {
   Navigate,
 } from "react-router-dom";
 import ExercisePage from "./pages/ExercisePage";
+import AdminPage from "./pages/AdminsPage";
+import CategoriesPage from "./pages/CategoriesPage";
 
 function App() {
   return (
     <main className="w-full h-full">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/exercises" />} />
+          <Route path="/" element={<Navigate to="/admins" />} />
+          <Route path="/admins" element={<AdminPage />} />
+          <Route path="/categories" element={<CategoriesPage />} />
           <Route path="/exercises" element={<ExercisePage />} />
         </Routes>
       </Router>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,12 +2,12 @@ import { FiLogOut, FiFileText } from "react-icons/fi";
 import { LuUsers } from "react-icons/lu";
 import { Link } from "react-router-dom"; // Import the Link component
 import SidebarLink from "./SidebarLink";
+import { BsBoxFill } from "react-icons/bs";
 
 const Sidebar = ({ activeRoute }: { activeRoute: string }) => {
   return (
     <div className="w-16 h-screen flex flex-col justify-between py-4 bg-navbar-color">
       <div>
-        {/* Logo */}
         <div className="flex justify-center mt-4 mb-8">
           <div className="bg-custom-light-blue rounded-lg w-12 h-12 flex items-center justify-center">
             <span className="text-custom-blue text-3xl font-bold">K</span>
@@ -15,8 +15,8 @@ const Sidebar = ({ activeRoute }: { activeRoute: string }) => {
         </div>
 
         <SidebarLink
-          active={activeRoute === "profile"}
-          to="/profile"
+          active={activeRoute === "admins"}
+          to="/admins"
           icon={LuUsers}
         />
 
@@ -25,9 +25,14 @@ const Sidebar = ({ activeRoute }: { activeRoute: string }) => {
           to="/exercises"
           icon={FiFileText}
         />
+
+        <SidebarLink
+          active={activeRoute === "categories"}
+          to="/categories"
+          icon={BsBoxFill}
+        />
       </div>
 
-      {/* Logout button at the bottom */}
       <div className="flex justify-center pb-4">
         <Link
           to="/login"
