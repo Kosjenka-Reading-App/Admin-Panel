9ba33a107b0cbdd64ccda92607e0cd432263e760
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-11-20 23:45:43 +0100

    Handling conflicting errors for existing emails

--- src/components/AdminForm.tsx
+++ src/components/AdminForm.tsx
@@ -7,20 +7,28 @@ const AdminForm: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isSuperAdmin, setIsSuperAdmin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // State to hold the error message
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setErrorMessage(""); // Clear any existing error messages
     adminService
       .create(email, password, isSuperAdmin)
       .then(() => {
         navigate("/admins");
       })
       .catch((err) => {
-        console.log(err);
+        // Check the error and set an appropriate message
+        if (err.response && err.response.status === 409) {
+          setErrorMessage("An account with this email already exists.");
+        } else {
+          setErrorMessage("An unexpected error occurred. Please try again.");
+        }
       });
   };
+  
 
   return (
     <div className="bg-white min-h-screen flex flex-col">
@@ -31,12 +39,15 @@ const AdminForm: React.FC = () => {
       </div>
       <div className="flex-grow flex items-center justify-center p-8">
         <div className="bg-custom-light-grey p-12 rounded-lg shadow-xl w-full max-w-2xl">
+          {/* Show error message when there is one */}
+          {errorMessage && (
+            <div className="mb-4 text-center p-2 text-red-600 bg-red-100 rounded">
+              {errorMessage}
+            </div>
+          )}
           <form onSubmit={handleSubmit} className="space-y-8">
             <div>
-              <label
-                htmlFor="email"
-                className="text-lg font-semibold text-gray-700 block"
-              >
+              <label htmlFor="email" className="text-lg font-semibold text-gray-700 block">
                 Email address
               </label>
               <input
@@ -44,16 +55,13 @@ const AdminForm: React.FC = () => {
                 id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="text-custom-black mt-1 px-4 py-3 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600 block w-full rounded-md text-lg"
+                className="mt-1 px-4 py-3 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600 block w-full rounded-md text-lg"
                 placeholder="example@email.com"
                 required
               />
             </div>
             <div>
-              <label
-                htmlFor="password"
-                className="text-lg font-semibold text-gray-700 block"
-              >
+              <label htmlFor="password" className="text-lg font-semibold text-gray-700 block">
                 Password
               </label>
               <input
@@ -61,7 +69,7 @@ const AdminForm: React.FC = () => {
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="text-custom-black mt-1 px-4 py-3 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600 block w-full rounded-md text-lg"
+                className="mt-1 px-4 py-3 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600 block w-full rounded-md text-lg"
                 placeholder="********"
                 required
               />
@@ -74,10 +82,7 @@ const AdminForm: React.FC = () => {
                 onChange={(e) => setIsSuperAdmin(e.target.checked)}
                 className="h-5 w-5 text-blue-600 border-gray-300 focus:ring-blue-500 rounded"
               />
-              <label
-                htmlFor="super-admin"
-                className="ml-2 text-lg font-semibold text-gray-900"
-              >
+              <label htmlFor="super-admin" className="ml-2 text-lg font-semibold text-gray-900">
                 Super admin privileges
               </label>
             </div>
