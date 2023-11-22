53f98a06d2fbe6b4d3b018a3f3a7f9ca365b4362
Satvik26 <satvikbisht26@gmail.com>
2023-11-22 14:50:56 +0100

    Added exercise form component for reusability

---
+++ src/services/logout.tsx
@@ -0,0 +1,12 @@
+import Cookies from "js-cookie";
+
+
+const logOut = () => {
+   
+        Cookies.remove('accessToken');
+        Cookies.remove('refreshToken');
+    
+   
+}
+
+export default logOut;
