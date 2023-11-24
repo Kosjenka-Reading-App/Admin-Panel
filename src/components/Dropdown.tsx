2b4ce18ff4908911b89aade0de57cf7c806998bd
Satvik26 <satvikbisht26@gmail.com>
2023-11-24 23:39:15 +0100

    Modified the dropdown text for better visibility

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,56 +3,65 @@ import Select from 'react-select';
 type ExerciseFormProps = {
     complexity: string;
     setComplexity: React.Dispatch<React.SetStateAction<string>>
-    
-  }
 
-const Dropdown = ({ complexity,  setComplexity }: ExerciseFormProps) => {
+}
+
+const Dropdown = ({ complexity, setComplexity }: ExerciseFormProps) => {
 
     const options = [
         { value: 'Easy', label: 'EASY', color: 'custom-green' },
-        { value: 'Medium', label: 'MEDIUM', color: 'custom-yelow' },
+        { value: 'Medium', label: 'MEDIUM', color: 'custom-yellow' },
         { value: 'Hard', label: 'HARD', color: 'custom-red' },
-      ];
-    
-      const getColorBasedOnValueText = (value: string) => {
+    ];
+
+    const getColorBasedOnValueText = (value: string) => {
         switch (value) {
-          case 'Easy':
-            return 'green';
-          case 'Medium':
-            return 'yellow';
-          case 'Hard':
-            return 'red';
-    
+            case 'Easy':
+                return '#10CA00';
+            case 'Medium':
+                return '#FFD700';
+            case 'Hard':
+                return '#FF0000';
+
         }
-      };
-  return (
-    <Select
-                options={options}
-                value={options.find((option) => option.value === complexity)}
-                onChange={(selectedOption) => setComplexity(selectedOption?.value || '')}
-                styles={{
-                  control: (provided) => ({
+
+
+    };
+
+
+    return (
+        <Select
+            options={options}
+            value={options.find((option) => option.value === complexity)}
+            onChange={(selectedOption) => setComplexity(selectedOption?.value || '')}
+            styles={{
+                control: (provided) => ({
                     ...provided,
-                     fontSize: '.9rem',
-                     height: '2.65rem',
-                     border: '.09rem solid #0099FF',
-                     boxShadow: '0 .4rem 1rem rgba(0, 0, 0, 0.37), 0 0 .1rem rgba(0, 0, 0, 0.37)',
-                     borderRadius: '7px',
-                     borderColor: 'custom-blue',
-                     //backgroundColor:'green',
+                    fontSize: '.9rem',
+                    height: '2.65rem',
+                    border: '.09rem solid #0099FF',
+                    boxShadow: '0 .4rem 1rem rgba(0, 0, 0, 0.37), 0 0 .1rem rgba(0, 0, 0, 0.37)',
+                    borderRadius: '7px',
+                    borderColor: 'custom-blue',
                     ':hover': {
                         borderColor: '#0099FF',
-                       },
+                    },
 
-                  }),
-                  option: (provided, state) => ({
+
+                }),
+                option: (provided, state) => ({
                     ...provided,
                     color: state.isSelected || state.isFocused ? getColorBasedOnValueText(state.data.value) : getColorBasedOnValueText(state.data.value),
-                                     
-                  }),
-                }}
-              />
-  )
+                    fontWeight: 'bold',
+                    background: state.isSelected ? 'transparent' : 'white',
+                    ':hover': {
+                        backgroundColor: '#DEF2FF',
+                    },
+
+                }),
+            }}
+        />
+    )
 }
 
 export default Dropdown
