c7b0762f475ae788bc973790f92673095eac963b
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-12-05 00:39:00 +0100

    Creating dropwdown for selecting category

--- src/components/CreateExercise.tsx
+++ src/components/CreateExercise.tsx
@@ -1,20 +1,47 @@
 import ExerciseForm from "./ExerciseForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import exerciseService from "../services/exercises";
 import { useNavigate } from "react-router-dom";
+import categoriesService from "../services/categories"; 
+
+type CategoryOption = {
+  value: string;
+  label: string;
+};
 
 const CreateExercise = () => {
-
-  const [title, setTitle] = useState("");
-  const [complexity, setComplexity] = useState("");
-  const [textExercise, setTextExercise] = useState("");
+  const [title, setTitle] = useState<string>("");
+  const [complexity, setComplexity] = useState<string>("");
+  const [textExercise, setTextExercise] = useState<string>("");
+  // Initialize categories with the correct type
+  const [categories, setCategories] = useState<CategoryOption[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
   const navigate = useNavigate();
 
-
+  useEffect(() => {
+    // Set default values for the parameters
+    const defaultPage = 1;
+    const defaultPerPage = 10;
+    const defaultSearchQuery = '';
+    const defaultSortField = '';
+    const defaultSortDir = 'asc'; // or 'desc' or '', depending on your API
+  
+    categoriesService.list(defaultPage, defaultPerPage, defaultSearchQuery, defaultSortField, defaultSortDir)
+      .then((response) => {
+        const categoryOptions: CategoryOption[] = response.data.map((item: any) => ({
+          value: item.category,
+          label: item.category,
+        }));
+        setCategories(categoryOptions);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch categories", error);
+      });
+  }, []);
+  
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     exerciseService
       .create(title, textExercise, complexity)
       .then(() => {
@@ -26,13 +53,23 @@ const CreateExercise = () => {
   };
 
 
-
   return (
     <div className="w-full h-screen">
       <div className="bg-custom-light-grey py-3 px-4 mb-5">
         <h1 className="font-bold text-2xl">Create Exercise</h1>
       </div>
-      <ExerciseForm onSubmit={handleSubmit} setTitle={setTitle} setComplexity={setComplexity} setTestExercise={setTextExercise} title={title} complexity={complexity} textExercise={textExercise} />
+      <ExerciseForm
+        onSubmit={handleSubmit}
+        setTitle={setTitle}
+        setComplexity={setComplexity}
+        setTextExercise={setTextExercise}
+        title={title}
+        complexity={complexity}
+        textExercise={textExercise}
+        categories={categories}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
     </div>
   );
 };
--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -1,73 +1,107 @@
-import React, { FormEventHandler } from 'react'
+import React from 'react';
 import Dropdown from './Dropdown';
+import Select from 'react-select';
+
+type CategoryOption = {
+  value: string;
+  label: string;
+};
 
 type ExerciseFormProps = {
-  onSubmit: FormEventHandler<HTMLFormElement>;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
   title: string;
   complexity: string;
   textExercise: string;
-  setTitle: React.Dispatch<React.SetStateAction<string>>
-  setComplexity: React.Dispatch<React.SetStateAction<string>>
-  setTestExercise: React.Dispatch<React.SetStateAction<string>>
-}
+  setTitle: React.Dispatch<React.SetStateAction<string>>;
+  setComplexity: React.Dispatch<React.SetStateAction<string>>;
+  setTextExercise: React.Dispatch<React.SetStateAction<string>>;
+  categories: CategoryOption[];
+  selectedCategory: string;
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
+};
 
-const ExerciseForm = ({ onSubmit, title, complexity, textExercise, setTitle, setComplexity, setTestExercise }: ExerciseFormProps) => {
+const ExerciseForm = ({
+  onSubmit,
+  title,
+  complexity,
+  textExercise,
+  setTitle,
+  setComplexity,
+  setTextExercise,
+  categories,
+  selectedCategory,
+  setSelectedCategory
+}: ExerciseFormProps) => {
 
+  const handleCategoryChange = (selectedOption: CategoryOption | null) => {
+    setSelectedCategory(selectedOption ? selectedOption.value : '');
+  };
 
   return (
-
     <form onSubmit={onSubmit}>
       <div className="flex flex-col mt-10 items-center justify-center h-3/4 w-3/4 m-auto shadow-lg shadow-custom-grey sm:max-w-[900px] bg-custom-light-grey">
         <div className="flex h-1/5 w-3/4 mt-0 m-auto ">
           <div className="flex-1 p-4 ">
             <div>
-              <label className="block mt-2 mb-3 px-1 text-3xl  font-bold text-custom-black">
+              <label className="block mt-2 mb-3 px-1 text-3xl font-bold text-custom-black">
                 Title
               </label>
               <input
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 type="text"
-                name="email"
-                id="email"
-                className=" px-3 mb-4 shadow-lg shadow-gray-400 border border-custom-blue text-custom-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                id="title"
+                className="px-3 mb-4 shadow-lg shadow-gray-400 border border-custom-blue text-custom-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                 placeholder="Add Title"
+                required
               />
             </div>
           </div>
           <div className="flex-1 p-4">
             <div>
-              <label className="block mt-2 mb-3 px-1 text-3xl  font-bold text-custom-black">
+              <label className="block mt-2 mb-3 px-1 text-3xl font-bold text-custom-black">
                 Complexity
               </label>
               <Dropdown complexity={complexity} setComplexity={setComplexity}/>
             </div>
           </div>
         </div>
-        <div className="flex-1 p-4  h-1/2 w-3/4 m-auto">
+        <div className="flex-1 p-4 w-3/4">
           <div>
-            <label className="block mt-2 mb-6 px-1 text-3xl  font-bold text-custom-black">
-              Text
+            <label className="block mt-2 mb-3 px-1 text-3xl font-bold text-custom-black">
+              Category
             </label>
-
-            <textarea
-              className="w-full text-sm font-sans font-normal leading-5 px-6 py-2 rounded-lg shadow-lg shadow-gray-400 border border-custom-blue text-custom-black sm:text-sm focus:ring-primary-600 focus:border-primary-600 block
-        focus-visible:outline-0 overflow-y-auto"
-              placeholder="Please enter your exercise"
-              value={textExercise}
-              onChange={(e) => setTestExercise(e.target.value)}
-              style={{ minHeight: "300px" }}
+            <Select
+              options={categories}
+              value={categories.find(c => c.value === selectedCategory)}
+              onChange={handleCategoryChange}
+              className="mb-4 shadow-lg shadow-gray-400 border border-custom-blue text-custom-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+              placeholder="Select Category"
+              required
             />
           </div>
         </div>
-        <button className="w-1/5 justify-center  py-2 mb-10 bg-custom-blue hover:bg-blue-400 rounded-lg">
+        <div className="flex-1 p-4 w-3/4">
+          <div>
+            <label className="block mt-2 mb-6 px-1 text-3xl font-bold text-custom-black">
+              Text
+            </label>
+            <textarea
+              value={textExercise}
+              onChange={(e) => setTextExercise(e.target.value)}
+              className="w-full px-6 py-2 rounded-lg shadow-lg shadow-gray-400 border border-custom-blue text-custom-black sm:text-sm focus:ring-primary-600 focus:border-primary-600 block overflow-y-auto"
+              placeholder="Please enter your exercise"
+              style={{ minHeight: "300px" }}
+              required
+            />
+          </div>
+        </div>
+        <button type="submit" className="w-1/5 justify-center py-2 mb-10 bg-custom-blue hover:bg-blue-800 rounded-lg">
           Submit
         </button>
       </div>
     </form>
+  );
+};
 
-
-  )
-}
-
-export default ExerciseForm
+export default ExerciseForm;
