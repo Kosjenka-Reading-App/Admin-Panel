90635cf1851c48d83874561a04b065fbacc04c18
Satvik26 <satvikbisht26@gmail.com>
2023-12-06 14:51:23 +0100

    Added category form for reusability

---
+++ src/components/CategoryForm.tsx
@@ -0,0 +1,54 @@
+import React, { FormEventHandler } from 'react'
+import { Link } from "react-router-dom";
+
+type CategoryFormProps = {
+    handleSubmit: FormEventHandler<HTMLFormElement>;
+    name: string;
+    setName: React.Dispatch<React.SetStateAction<string>>
+
+}
+
+
+const CategoryForm = ({ handleSubmit, name, setName }: CategoryFormProps) => {
+
+    return (
+
+        <form onSubmit={handleSubmit} className="space-y-8">
+            <div>
+                <label
+                    htmlFor="name"
+                    className="text-lg font-semibold text-gray-700 block"
+                >
+                    Category Name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="text-custom-black mt-1 px-4 py-3 bg-white border border-slate-300 placeholder-slate-400 focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-600 block w-full rounded-md text-lg"
+                    placeholder="Enter category name"
+                    required
+                />
+            </div>
+            <div className="flex items-center justify-between mt-8">
+                <button
+                    type="submit"
+                    className="px-6 py-3 bg-custom-dark-blue text-white text-lg rounded hover:bg-blue-800"
+                >
+                    Save
+                </button>
+                <Link
+                    to="/categories"
+                    className="px-6 py-3 bg-custom-grey text-gray-800 text-lg rounded hover:bg-gray-400"
+                >
+                    Cancel
+                </Link>
+            </div>
+        </form>
+
+
+    )
+}
+
+export default CategoryForm
