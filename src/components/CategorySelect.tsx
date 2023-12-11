76ba59425a83210a15a784c7c159dd4d5dcb7863
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-12-11 20:19:28 +0100

    Fixed build

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AsyncSelect from "react-select/async";
 import categoriesService from "../services/categories"; // Ensure this is the correct path
 
