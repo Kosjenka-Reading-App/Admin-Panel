3abc96fe3f6a8e3b0b18bdc40ec0ab811e07667e
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-12-16 19:50:07 +0530

    Added tests

--- cypress/e2e/exerciseList.cy.ts
+++ cypress/e2e/exerciseList.cy.ts
@@ -1,4 +1,36 @@
-// Create a test for the ExerciseList Page to check the Simple list, page count, filters, sorts, pagination and the colors of the label
-
+import { exerciseList } from '../intercepts/exerciseList';
 import { meSuperadmin } from "../intercepts/login";
-import { exerciseList } from "../intercepts/exerciseList";
+import { createExercise } from "../intercepts/createExercise";
+import { listCategories } from "../intercepts/categories";
+
+describe("Exercise List Page", () => {
+    beforeEach(() => {
+        meSuperadmin();
+        cy.visit('http://localhost:5173/exercises');
+    });
+
+    it("shows the main elements of the page", () => {
+        cy.contains('h1', 'Exercises').should('be.visible');
+        cy.get('input[name="query"]').should('be.visible');
+        cy.get('a').contains('New exercise').should('be.visible');
+    });
+
+    // it("shows exercise table with expected columns", () => {
+    //     cy.get('.rdt_TableCol').should('contain', '#');
+    //     cy.get('.rdt_TableCol').should('contain', 'Title');
+    //     cy.get('.rdt_TableCol').should('contain', 'Category');
+    //     cy.get('.rdt_TableCol').should('contain', 'Complexity');
+    //     cy.get('.rdt_TableCol').should('contain', 'Last update');
+    //     cy.get('.rdt_TableCol').should('contain', 'Actions');
+    // });
+
+    it("allows navigation to the create exercise page", () => {
+        cy.get('a').contains('New exercise').click();
+        cy.url().should('include', '/exercises/create');
+    });
+    // Additional tests can be added as needed
+    it("displays the correct title count", () => {
+        cy.get('h1').contains('Exercises').should('be.visible');
+        cy.get('h1').contains('Exercises').should('have.length', 1);
+    })
+    });
--- cypress/intercepts/exerciseList.ts
+++ cypress/intercepts/exerciseList.ts
@@ -1,5 +1,17 @@
-const exerciseList = () => {
-    cy.intercept('GET', '/api/exercises', { fixture: 'exerciseList.json' })
-        .as('exerciseList');
-}
-export { exerciseList };
+export const exerciseList = () => {
+    return cy.intercept('GET', '/exercises?page=1&size=100&title_like=&complexity_like=&categories_like=', {
+        statusCode: 200,
+        body: {
+            items: [
+                {
+                    title: 'Exercise 1',
+                    complexity: 'Easy',
+                    categories: ['Category 1', 'Category 2'],
+                    completion : 0,
+                    position : 0,
+                    time_spent : 0,
+                },
+            ],
+        },
+    });
+};
