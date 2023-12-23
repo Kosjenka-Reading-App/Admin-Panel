83aaf36d608c6bdc9c40246d2ffe7cc5b65e3a50
Rishabh Tiwari <rishabh.tiwari@stud.uni-heidelberg.de>
2023-12-24 01:06:34 +0530

    added test

--- cypress/intercepts/exerciseList.ts
+++ cypress/intercepts/exerciseList.ts
@@ -15,3 +15,21 @@ export const exerciseList = () => {
         },
     });
 };
+
+export const filters = () => {
+    return cy.intercept('GET', '/exercises?page=1&size=100&title_like=&complexity_like=&categories_like=', {
+        statusCode: 200,
+        body: {
+            items: [
+                {
+                    title: 'Exercise 1',
+                    complexity: 'Easy',
+                    categories: ['Category 1', 'Category 2'],
+                    completion : 0,
+                    position : 0,
+                    time_spent : 0,
+                },
+            ],
+        },
+    });
+}
